1bfd10ed7c7ade95bda0030408f5ccca5f114702
SungKyum Kim <sungkyum1@naver.com>
2023-12-14 16:51:11 +0900

    feat(RandomNumberGenerator): 1-5 사이의 랜덤한 숫자를 반환하는 클래스 구현

---
+++ src/main/java/menu/util/RandomNumberGenerator.java
@@ -0,0 +1,12 @@
+package menu.util;
+
+import camp.nextstep.edu.missionutils.Randoms;
+
+public class RandomNumberGenerator {
+    private static final int MIN_NUMBER = 1;
+    private static final int MAX_NUMBER = 1;
+
+    public static int generate() {
+        return Randoms.pickNumberInRange(MIN_NUMBER, MAX_NUMBER);
+    }
+}
