687a21a7c23266172cf9b51227cce77b4df7b140
SungKyum Kim <sungkyum1@naver.com>
2023-12-14 17:25:12 +0900

    feat(Catetory): 중복을 2회까지만 허용하는 5개의 랜덤 카테고리 생성 기능 구현

--- src/main/java/menu/domain/Category.java
+++ src/main/java/menu/domain/Category.java
@@ -1,5 +1,48 @@
 package menu.domain;
 
+import menu.util.RandomNumberGenerator;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
 public enum Category {
-    일식, 한식, 중식, 아시안, 양식
+    일식(1), 한식(2), 중식(3), 아시안(4), 양식(5);
+
+    private final int order;
+    private static final int maxDuplicates = 2;
+    private static final int categorySize = 5;
+
+    Category(int order) {
+        this.order = order;
+    }
+
+    public static List<Category> createRandomCategories() {
+        List<Category> randomCategories = new ArrayList<>();
+
+        while (randomCategories.size() < categorySize) {
+            int randomOrder = RandomNumberGenerator.generate();
+
+            // 중복 체크
+            int duplicateCount = Collections.frequency(randomCategories, getCategoryByOrder(randomOrder));
+            if (duplicateCount < maxDuplicates) {
+                randomCategories.add(getCategoryByOrder(randomOrder));
+            }
+        }
+
+        return randomCategories;
+    }
+
+    private static Category getCategoryByOrder(int order) {
+        for (Category category : Category.values()) {
+            if (category.order == order) {
+                return category;
+            }
+        }
+        throw new IllegalArgumentException("Invalid order: " + order);
+    }
+
+    public int getOrder() {
+        return order;
+    }
 }
