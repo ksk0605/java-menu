b1648bb8db21decb43bf0e1219651f330a9efc2d
SungKyum Kim <sungkyum1@naver.com>
2023-12-14 16:10:16 +0900

    feat(InedibleMenus): 못 먹는 메뉴 목록 클래스, 검증 로직 구현

--- src/main/java/menu/domain/InedibleMenus.java
+++ src/main/java/menu/domain/InedibleMenus.java
@@ -1,2 +1,38 @@
-package menu.domain;public class InedibleMenus {
+package menu.domain;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class InedibleMenus {
+    private static final int MAX_INEDIBLE_MENUS = 2;
+
+    List<Menu> menus;
+
+    public InedibleMenus(String menus) {
+        this.menus = new ArrayList<>();
+        if (!menus.isEmpty()) {
+            validateAndCreateMenus(split(menus));
+        }
+    }
+
+    private void validateAndCreateMenus(String[] menus) {
+        if (menus.length > MAX_INEDIBLE_MENUS) {
+            throw new IllegalArgumentException();
+        }
+        createMenus(menus);
+    }
+
+    private void createMenus(String[] menus) {
+        for (String menu : menus) {
+            this.menus.add(Menu.valueOf(menu.trim()));
+        }
+    }
+
+    private String[] split(String menus) {
+        return menus.split(",");
+    }
+
+    public List<Menu> getMenus() {
+        return menus;
+    }
 }
