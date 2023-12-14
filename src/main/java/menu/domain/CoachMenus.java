cfb7793d60a1577ad65f1c9a4bbd5d06e0b80e2a
SungKyum Kim <sungkyum1@naver.com>
2023-12-14 17:28:55 +0900

    feat(CoachMenus): 랜덤한 메뉴 5개를 추천하는 기능 구현

--- src/main/java/menu/domain/CoachMenus.java
+++ src/main/java/menu/domain/CoachMenus.java
@@ -1,5 +1,36 @@
 package menu.domain;
 
-public class MenuRecommender {
-    public
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
+public class CoachMenus {
+    private final Coach coach;
+    private final Set<String> menus;
+
+    public CoachMenus(Coach coach, List<Category> categories) {
+        this.coach = coach;
+        this.menus = new HashSet<>();
+        createMenus(categories);
+    }
+
+    public void createMenus(List<Category> categories) {
+        for (Category category : categories) {
+            String randomMenu = Menu.createRandomMenuByCategory(category);
+
+            while (menus.contains(randomMenu) || coach.isInedibleMenu(Menu.valueOf(randomMenu))) {
+                randomMenu = Menu.createRandomMenuByCategory(category);
+            }
+
+            menus.add(randomMenu);
+        }
+    }
+
+    public Coach getCoach() {
+        return coach;
+    }
+
+    public Set<String> getMenus() {
+        return menus;
+    }
 }
