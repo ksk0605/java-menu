fdd76e6e41c8acccf07a2373fcc8aef867e13323
SungKyum Kim <sungkyum1@naver.com>
2023-12-14 15:06:40 +0900

    feat(CoachName): 코치 이름 클래스 구현, 코치 이름 검증 기능 구현

---
+++ src/main/java/menu/domain/CoachName.java
@@ -0,0 +1,23 @@
+package menu.domain;
+
+public class CoachName {
+    private static final int MIN_LENGTH = 2;
+    private static final int MAX_LENGTH = 4;
+
+    String name;
+
+    public CoachName(String name) {
+        validateName(name);
+        this.name = name;
+    }
+
+    public void validateName(String name) {
+        if (isOutOfRangeName(name)) {
+            throw new IllegalArgumentException();
+        }
+    }
+
+    private static boolean isOutOfRangeName(String name) {
+        return name.length() < MIN_LENGTH || name.length() > MAX_LENGTH;
+    }
+}
